b8ec2f2e4298c384cf8c75b747a7c226ad17e642
Ariful Alam <arifulalam@users.noreply.github.com>
2016-04-02 02:33:23 +0600

    update #1

--- RandomPassword.php
+++ RandomPassword.php
@@ -1,39 +1,41 @@
 <?php
-	class RandomPassword{
-		function generateCode($characters, $mix) {
-			/* list all possible characters, similar looking characters and vowels have been removed */
-			$alpha = 'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ';
-			$numeric = '0123456789';
-			$special = '!@#$%^()[]';
+class RandomPassword{
+	/* list all possible characters, similar looking characters and vowels have been removed */
+	public $alpha = 'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ';
+	public $numeric = '0123456789';
+	public $special = '!@#$%^()[]';
+	
+	public $code;
+	public $alphanumeric;
+	public $power;
+	public $characterSet;
+	
+	function __construct($characters = 12, $mix = 'alphanumeric') {
+		$this->alphanumeric = $this->numeric . $this->alpha;
+		$this->power = $this->alphanumeric . $this->special;
 
-			$alphanumeric = $numeric . $alpha;
-			$power = $alphanumeric . $special;
+		// Used to loop the while statement
+		$i = 0;
 
-			// Will store the required generated code
-			$code = '';
+		// Checks which character set is d and sets it to $characterSet
+		if($mix === 'alphanumeric'){
+			$this->characterSet = $this->alphanumeric;
+		}else if($mix === 'alpha'){
+			$this->characterSet = $this->alpha;
+		}else if($mix === 'numeric'){
+			$this->characterSet = $this->numeric;
+		}else if($mix === 'mix'){
+			$this->characterSet = $this->power;
+		}
 
-			// Used to loop the while statement
-			$i = 0;
-
-			// Will store the required character set for the random code
-			$characterSet;
-
-			// Checks which character set is required and sets it to $characterSet
-			if($mix === 'alphanumeric'){
-				$characterSet = $alphanumeric;
-			}else if($mix === 'alpha'){
-				$characterSet = $aplha;
-			}else if($mix === 'numeric'){
-				$characterSet = $numeric;
-			}else if($mix === 'mix'){
-				$characterSet = $power;
-			}
-
-			// Generates new random code and return it
-			while ($i < $characters) {
-				$code .= substr($characterSet, mt_rand(0, strlen($characterSet)-1), 1);
-				$i++;
-			}
-			return $code;
+		// Generates new random code and return it
+		while ($i < $characters) {
+			$this->code .= substr($this->characterSet, mt_rand(0, strlen($this->characterSet)-1), 1);
+			$i++;
 		}
 	}
+	
+	function __toString(){
+		return $this->code;	
+	}
+}
